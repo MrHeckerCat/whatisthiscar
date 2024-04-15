bfb8ae1153fbb5c31e9a7fab2d1fa8f777e8d11e
MrHeckerCat <129594453+MrHeckerCat@users.noreply.github.com>
2024-04-15 18:06:57 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,8 @@
-const express = require('express');
-const app = express();
-const path = require('path');
+const app = require('./app');
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-// Listen to the App Engine-specified port, or 8080 otherwise
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}...`);
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
 
 
