661e94f4af0f83a485dcbec247cfdb33fff8ab0f
MrHeckerCat <129594453+MrHeckerCat@users.noreply.github.com>
2024-09-20 20:07:29 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const app = require('./app');
-const path = require('path'); // Include the path module
+const path = require('path');
 
-// ... other code ... 
+// Middleware to handle www to non-www redirect
+app.use((req, res, next) => {
+  if (req.hostname === 'www.whatisthicar.io') {
+    return res.redirect(301, `${req.protocol}://whatisthicar.io${req.originalUrl}`);
+  }
+  next();
+});
 
 // Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public'))); 
+app.use(express.static(path.join(__dirname, 'public')));
+
+// ... other routes and middleware ...
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
-
-
-
